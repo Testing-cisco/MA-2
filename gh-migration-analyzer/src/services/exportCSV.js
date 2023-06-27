81545b875d868c3a20dddb13e29c4c3667da12cd
Gascisco <gnjofang@releaseteam.com>
2023-06-27 03:13:16 -0500

    Add script to recursively run the analyzer againt a list of file

---
+++ gh-migration-analyzer/src/services/exportCSV.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+import { createObjectCsvWriter as createCsvWriter } from "csv-writer";
+
+/**
+ * To store metics in a CSV file
+ * @param {string} path the name of repository
+ * @returns {object} the headers of CVS file
+ */
+export const csvExporter = (path, header, append) =>
+  createCsvWriter({
+    path,
+    header,
+    append,
+    headerIdDelimiter: ".",
+  });
---
+++ launch.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo -e "########### Starting Script ###################"
+sleep 3
+
+echo "Collecting Data"
+filename=$1
+auth_token=$2
+echo -e "########### Starting Migration Analyzer #############"
+# Generate files for the report
+echo "orgName, repoName, pushedAt, isArchived, numOfPullRequests, numOfIssues, numOfProjects, numOfDiscussions, numOfPackages, numOfReleases, wikiEnabled, diskUsage" > ./metrics/repo-metrics.csv
+echo "orgName, numOfMembers, numOfProjects, numOfRepos, mostPrs, averagePrs, mostIssues, averageIssues" > ./metrics/org-metrics.csv
+# Read the file and run migration analyzer for each organization name
+is_header=true
+while read org
+do
+	if [ "$is_header" = true ]; then
+		is_header=false
+		continue
+	fi
+	echo "Running analyzer for $org"
+	node gh-migration-analyzer/src/index.js GH-org -o "$org" -t "$auth_token" -s https://api.github.com/graphql
+	echo "Analyzer completed for $org"
+done < "$filename"
